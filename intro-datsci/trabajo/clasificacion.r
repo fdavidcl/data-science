e737d48816f4c3719073e18b20e3bb495bea36a2
David Charte <fdavidcl@protonmail.com>
2018-01-08 19:52:29 +0100

    Trabajo intro datsci

--- intro-datsci/trabajo/clasificacion.r
+++ intro-datsci/trabajo/clasificacion.r
@@ -1,2 +1,178 @@
 #!/usr/bin/env Rscript
 
+#;--------------------- Carga de datos ---------------------
+#; Función para cargar el dataset completo y las particiones
+monk_load = function(filename) {
+  dat = RWeka::read.arff(filename)
+  dat
+}
+
+monk = monk_load("monk-2/monk-2.arff")
+
+#;-------------------- Análisis de datos --------------------
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(cowplot)
+library(corrplot)
+
+str(monk)
+
+#; Comprobación de falta de correlación
+pdf("21.pdf", width = 5, height = 5)
+corrplot(cor(monk[,-7]), type = "lower", method = "square")
+dev.off()
+
+#; Incoherencia con el problema descrito
+pdf("22.pdf", width = 4, height = 4)
+plot(Class ~ A5, data = monk)
+dev.off()
+
+#; Deduciendo el problema MONK
+monk1cl = 1 * (monk$A1 == monk$A2 | monk$A5 == 1)
+monk2cl = 1 * (rowSums(monk[, 1:6] == 1) == 2)
+monk3cl = 1 * ((monk$A5 == 3 & monk$A4 == 1) | (monk$A5 != 4 & monk$A2 != 3))
+mean(monk$Class == monk1cl) #; => 0.5463
+mean(monk$Class == monk2cl) #; => 0.4722
+mean(monk$Class == monk3cl) #; => 1
+
+#; Nivel de ruido
+mean(monk$Class != monk3cl) #; => 0
+
+#; Heatmap del dataset completo
+monk2 = monk
+monk2$Class = as.numeric(monk2$Class) - 1
+monk2$id = 1:432
+melted = reshape2::melt(monk2, id.vars = "id")
+melted$value = factor(melted$value)
+pdf("23.pdf", width = 5, height = 7)
+ggplot(melted, aes(variable, id, fill = value)) + 
+  geom_raster()
+dev.off()
+
+#; Proporciones de clases
+pdf("27.pdf", width = 3, height = 5)
+ggplot(monk, aes(Class, fill = Class)) + 
+  geom_bar() +
+  scale_fill_discrete(guide=FALSE)
+dev.off()
+
+#;===================== Clasificación =====================
+#;----------------- Funciones auxiliares ------------------
+acc = function(preds, truth) {
+  mean(preds == truth)
+}
+run_fold = function(train, test, method, ...) {
+  caretmethod = function(train, test) {
+    model = caret::train(Class ~ ., data = train,
+                         method = method,
+                         preProcess = c(), ...)
+    predict(model, newdata = test[, 1:6])
+  }
+  methods = list(
+    classknn = function(train, test) {
+      class::knn(train = train[, 1:6],
+                 test = test[, 1:6],
+                 cl = train$Class,
+                 ...)
+    },
+    kknn = function(train, test) {
+      model = kknn::kknn(Class ~ ., train, test, ...)
+      model$fitted.values
+    },
+    knn = caretmethod,
+    lda = caretmethod,
+    qda = caretmethod
+  )
+  #; calcular predicciones y su error cuadrático medio
+  c(
+    train = acc(methods[[method]](train, train), train$Class),
+    test  = acc(methods[[method]](train, test), test$Class)
+  )
+}
+crossval = function(basename, folds, method = "knn", ...) {
+  results = lapply(1:folds, function(i) {
+    #; carga de datos
+    train = monk_load(paste0(basename, "-", folds, "-", i, "tra.arff"))
+    test  = monk_load(paste0(basename, "-", folds, "-", i, "tst.arff"))
+    
+    #; accuracy del fold
+    run_fold(train, test, method, ...)
+  })
+  
+  #; cálculo de medias
+  cat("Acierto medio train:", 
+      mean(sapply(results, function(x) x["train"])), "\n")
+  cat("Acierto medio test: ", 
+      mean(sapply(results, function(x) x["test"])), "\n")
+  
+  results
+}
+
+#;-------------------------- kNN --------------------------
+set.seed(12345)
+
+#; Ejecuciones de kNN con k aumentando progresivamente
+allk = seq(1, 37, 2)
+knnruns = data.frame(
+  k = allk,
+  acc =
+    sapply(allk, function(k) {
+      cat("Usando k =", k, "\n")
+      knncv = crossval("monk-2/monk-2", 10, "classknn", k = k)
+      mean(sapply(knncv, function(x) x["test"]))
+    })
+)
+pdf("24.pdf", width = 7, height = 3.5)
+plot(knnruns, type = "o", pch = 20, ylim = c(0.9, 1))
+dev.off()
+
+knnfit = crossval("monk-2/monk-2", 10, "classknn", k = 5)
+
+#;-------------------------- LDA --------------------------
+#; Comprobamos la falta de normalidad
+sapply(monk[, 1:6], function(v) shapiro.test(v)$p.value)
+pdf("25.pdf", width = 5, height = 5)
+qqnorm(monk$A1)
+qqline(monk$A1, col = "red")
+dev.off()
+
+ldafit = crossval("monk-2/monk-2", 10, "lda")
+
+#;-------------------------- QDA --------------------------
+qdafit = crossval("monk-2/monk-2", 10, "qda")
+
+#; partition plot
+pdf("26.pdf", width = 7, height = 7)
+klaR::partimat(
+  Class ~ A2 + A4 + A5,
+  data = monk,
+  method = "qda",
+  plot.matrix = T
+)
+dev.off()
+
+#;---------------------- Comparación ----------------------
+#; Calcular los aciertos medios de cada algoritmo
+results = t(as.data.frame(lapply(list(knnfit, ldafit, qdafit), function(fit) {
+  c(
+    train = mean(sapply(fit, function(x) x["train"])),
+    test = mean(sapply(fit, function(x) x["test"]))
+  )
+}), col.names = c("knn", "lda", "qda")))
+
+#; Guardar los resultados en LaTeX
+sink("clas_res.tex")
+knitr::kable(results, format = "latex")
+sink()
+
+res_test = read.csv("clasif_test_alumos.csv", row.names = 1)
+res_train = read.csv("clasif_train_alumnos.csv", row.names = 1)
+
+#; Guardar la tabla de test en LaTeX
+sink("clas_res_all.tex")
+knitr::kable(res_test, format = "latex")
+sink()
+
+#; Comparar por Friedman
+friedman.test(as.matrix(res_test)) #; => p = 0.70
--- intro-datsci/trabajo/regresion.r
+++ intro-datsci/trabajo/regresion.r
@@ -40,11 +40,12 @@ dev.off()
 #; contar el número de instancias por número de anillos y sexo
 #; y construir un gráfico de barras
 pdf("02.pdf")
-abalone %>% 
+rcplot = abalone %>% 
   group_by(Sex) %>% count(Rings) %>%
   ggplot(aes(Sex, n, fill = factor(Rings))) +
   geom_col() + 
   scale_fill_discrete(name = "Rings")
+print(rcplot)
 dev.off()
 
 #; boxplot anillos respecto a sexo
@@ -74,9 +75,51 @@ pdf("12.pdf", width = 5, height = 5)
 corrplot(cor(abalone[, 2:9]), method = "number", type = "lower")
 dev.off()
 
-#;-------------------- Regresión --------------------
-#; Modelo lineal simple
+#;==================== Regresión ====================
+#;-------------- Funciones auxiliares ---------------
+mse = function(preds, truth) {
+  sum((truth - preds) ^ 2) / length(preds)
+}
+run_fold = function(train, test, method = "lm", ...) {
+  methods = list(
+    #; modelo lineal múltiple
+    lm = function(train, test) {
+      model = lm(Rings ~ ., data = train, ...)
+      predict(model, test)
+    },
+    #; kNN
+    knn = function(train, test) {
+      model = kknn::kknn(Rings ~ ., train, test, ...)
+      model$fitted.values
+    }
+  )
+  
+  #; calcular predicciones y su error cuadrático medio
+  c(
+    train = mse(methods[[method]](train, train), train$Rings),
+    test  = mse(methods[[method]](train, test), test$Rings)
+  )
+}
+crossval = function(basename, folds, method = "lm", ...) {
+  results = lapply(1:folds, function(i) {
+    #; carga de datos
+    train = abalone_load(paste0(basename, "-", folds, "-", i, "tra.arff"))
+    test  = abalone_load(paste0(basename, "-", folds, "-", i, "tst.arff"))
+    
+    #; errores del fold
+    run_fold(train, test, method, ...)
+  })
+  
+  #; cálculo de medias
+  cat("Error medio train:", 
+      mean(sapply(results, function(x) x["train"])), "\n")
+  cat("Error medio test: ", 
+      mean(sapply(results, function(x) x["test"])), "\n")
+  
+  results
+}
 
+#;----------------- Modelo lineal simple -----------------
 lmfit = list()
 lmfit[[1]] = lm(Rings ~ Sex, data = abalone)
 lmfit[[2]] = lm(Rings ~ Diameter, data = abalone)
@@ -90,14 +133,88 @@ plot(Rings ~ Shell_weight, data = abalone, pch = 20)
 abline(lmfit[[5]], col = "red")
 dev.off()
 
-#; Modelo lineal múltiple
-
+#;----------------- Modelo lineal múltiple -----------------
 multfit = lm(Rings ~ ., data = abalone)
 summary(multfit)
 
-multfit2 = lm(Rings ~ Diameter + Shucked_weight + Shell_weight, data = abalone)
+multfit2 = lm(Rings ~ Diameter + Shucked_weight + Shell_weight, 
+              data = abalone)
 summary(multfit2)
 
-#; kNN
-library(kknn)
-#; Comparación
+#; Otros intentos: no linealidad, interacciones
+multfit3 = lm(Rings ~ Length * Diameter * Height, 
+              data = abalone)
+summary(multfit3)
+
+multfit4 = lm(Rings ~ Diameter * Height + 
+                Sex * Shucked_weight + Shell_weight,
+              data = abalone)
+summary(multfit4)
+
+#;----------------------- kNN -----------------------
+set.seed(12345)
+knncv = crossval("abalone/abalone", 5, "knn")
+
+#; Varias ejecuciones de kNN
+allk = seq(1, 37, 2)
+knnruns = data.frame(
+  k = allk,
+  mse =
+    sapply(allk, function(k) {
+      cat("Usando k =", k, "\n")
+      knncv = crossval("abalone/abalone", 5, "knn", k = k)
+      mean(sapply(knncv, function(x) x["test"]))
+    })
+)
+pdf("14.pdf", width = 5, height = 5)
+plot(knnruns, type = "o", pch = 20, ylim = c(0, 9))
+dev.off()
+
+#;-------------------- Comparación --------------------
+lmfit = crossval("abalone/abalone", 5)
+knnfit = crossval("abalone/abalone", 5, "knn")
+
+res_test = read.csv("regr_test_alumnos.csv", row.names = 1)
+res_train = read.csv("regr_train_alumnos.csv", row.names = 1)
+
+#; guardo tabla de resultados en LaTeX
+sink("res_test.tex")
+knitr::kable(
+  res_test, format = "latex", 
+  format.args = list(scientific = FALSE, drop0trailing = T))
+sink()
+
+#; función que realiza un test de Wilcoxon
+wilcoxon = function(other, ref) {
+  difs = (other - ref) / other
+  wilc_1_2 = cbind(ifelse (difs < 0, abs(difs) + 0.1, 0 + 0.1),
+                   ifelse (difs > 0, abs(difs) + 0.1, 0 + 0.1))
+  the_test = wilcox.test(wilc_1_2[, 1],
+                         wilc_1_2[, 2],
+                         alternative = "two.sided",
+                         paired = TRUE)
+  inv_test = wilcox.test(wilc_1_2[, 2],
+                         wilc_1_2[, 1],
+                         alternative = "two.sided",
+                         paired = TRUE)
+  c(
+    Rmas = the_test$statistic,
+    Rmenos = inv_test$statistic,
+    pvalue = the_test$p.value
+  )
+}
+
+#; Aplicar el test a LM y kNN
+wilcoxon(res_test[, 1], res_test[, 2])
+
+#; Aplicar Friedman para comparación múltiple
+friedman.test(as.matrix(res_test))
+
+#; Aplicar test post-hoc de Holm
+tam <- dim(res_test)
+groups <- rep(1:tam[2], each=tam[1])
+pairwise.wilcox.test(as.matrix(res_test),
+                     groups,
+                     p.adjust = "holm",
+                     paired = TRUE)
+
